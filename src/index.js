013cf29d4d507c7a7c38b8a859c2bfa61790746f
elseElseIf <mihail.pryka@gmail.com>
2022-10-06 23:23:21 +0300

    feat: add header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const { App } = require("./js components/app");
+
+window.onload = function () {
+  const app = new App();
+
+  let newContainer = document.createElement("div");
+  newContainer.id = "mainContainer";
+  newContainer.classList.add("container");
+  document.body.appendChild(newContainer);
+
+  newContainer.innerHTML = app.render();
+};
---
+++ src/js components/app.js
@@ -0,0 +1,35 @@
+export class App {
+  render() {
+    return `
+      <header>
+      <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="./src/img/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      </header>
+      <main>
+      </main>
+      <section>
+      </section>
+      `;
+  }
+}
